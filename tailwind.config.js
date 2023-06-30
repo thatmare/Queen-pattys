8f6eba7f7af092e5fae5dbbfb0d09f5635169007
BCAndreaC <bc.andreac@gmail.com>
2023-06-30 09:29:10 -0500

    Vista Login
    Co-authored-by: Marissa Vargas <marissa.lorena@hotmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        celadon: '#C9F0E0',
+        gunMetal: '#1D2939',
+        blackInput: '#252D37',
+        strokeInput: '#D4C5C5',
+      },
+    },
+  },
+  plugins: [],
+}
