10a192f4623e5ef619e0ad992ff80954cee99c74
BCAndreaC <bc.andreac@gmail.com>
2023-07-13 13:06:42 -0500

    Modal

--- src/Components/Kitchen/kitchen.tsx
+++ src/Components/Kitchen/kitchen.tsx
@@ -1,7 +1,9 @@
 import { Logo } from "../Login/Login.components.tsx";
 import { getOrders } from "../../Services/getOrders.tsx";
 import { patchOrders } from "../../Services/patchOrders.tsx";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment, useRef } from "react";
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
 interface Order {
   id: number;
@@ -19,8 +21,88 @@ interface Order {
   }[];
 }
 
+function Modal({ selectedOrderID, onClose, onCompleted } : { selectedOrderID: number | null; onClose: () => void; onCompleted: (orderID: number) => void; }) {
+  const [open, setOpen] = useState(true);
+
+  const cancelButtonRef = useRef(null);
+
+  return (
+    <Transition.Root show={open && selectedOrderID !== null} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                        Deactivate account
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Are you sure you want to deactivate your account? All of your data will be permanently
+                          removed. This action cannot be undone.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    onClick={() => {
+                      onCompleted(selectedOrderID); 
+                      onClose()
+                    }}
+                  >
+                    Completado
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
+
 export function Kitchen() {
   const [orders, setOrders] = useState<Order[]>([]);
+  const [selectedOrderID, setSelectedOrderID] = useState<number | null>(null);
   const kitchenOrders = orders;
 
   useEffect(() => {
@@ -33,6 +115,16 @@ export function Kitchen() {
         console.error("ERROR DE GET ORDERS", error);
       });
   }, []);
+
+  function handleOrderCompleted(orderID: number) {
+    patchOrders(orderID)
+      .then(() => {
+        setOrders(o => o.filter(order => order.id !== orderID))
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
 
   return (
     <section className="flex flex-col justify-evenly items-start bg-gunMetal min-h-screen min-w-fit max-w-screen">
@@ -68,11 +160,15 @@ export function Kitchen() {
               <button
                 type="button"
                 className="font-medium text-black rounded-md bg-celadon p-2"
-                onClick={() => patchOrders(order.id)}
+                onClick={ () => setSelectedOrderID(order.id)}
               >
                 Completado
               </button>
             </div>
+            <Modal
+              selectedOrderID={selectedOrderID}
+              onClose={() => setSelectedOrderID(null)}
+              onCompleted={handleOrderCompleted}></Modal>
           </div>
           <br />
         </div>
