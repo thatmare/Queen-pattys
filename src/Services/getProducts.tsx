0a54f7f1333d2a71cb738fb421706524d2572afc
BCAndreaC <bc.andreac@gmail.com>
2023-07-21 12:51:46 -0500

    Style: Products modal edit and delete

--- src/Services/getProducts.tsx
+++ src/Services/getProducts.tsx
@@ -1,5 +1,5 @@
 
-export function fetchProducts () {
+function fetchProducts () {
     const token = localStorage.getItem('token');
     return fetch('http://localhost:8080/products', {
         headers: {
@@ -13,3 +13,70 @@ export function fetchProducts () {
         console.error('Error fetching products:', error)
     })
 }
+
+function postProducts(name: string, price: number, type: string) {
+    const token = localStorage.getItem('token');
+    return fetch('http://localhost:8080/products', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            name: name,
+            price: price,
+            type: type,
+        })
+    })
+    .then(response =>{
+        return response.json()
+    })
+    .then(data => {
+        console.log(data, 'AQUI NUEVO PRODUCTO!!')
+    })
+    .catch(error =>{
+        console.error('Error sending product:', error);
+    });
+}
+
+
+function deleteProduct(id: number){
+    const token = localStorage.getItem('token');
+    return fetch(`http://localhost:8080/products/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'contentType': 'application/json',
+            'authorization': `Bearer ${token}`
+        }
+})
+.then(response =>{
+    return response.json()
+})
+.catch(error =>{
+    console.error('Error deleting product:', error);
+});
+}
+
+function patchProducts(id: number, name: string, price: number, type: string) {
+    const token = localStorage.getItem('token');
+    return fetch(`http://localhost:8080/products/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'content-type': 'application/json',
+            'authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            name: name, 
+            price: price,
+            type: type,
+        })
+    })
+    .then(response =>{
+        return response.json()
+    })
+    .catch(error =>{
+        console.error('Error updating product:', error);
+    });
+}
+
+export { fetchProducts, postProducts, deleteProduct, patchProducts };
